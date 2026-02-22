3108ffbff428480f704c2fd47873a87905146c0e
Tharaniben1 <tharanben193@gamil.com>
2026-02-22 22:17:34 +0530

    testing with multiple file changes in new-bug

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log ("Welcome to git and github course");
 console.log ("Welcome to git and github course");
 console.log ("Welcome to git and github course");
+
+for (var i=0 ; i<5 ; i++){
+    console.log("hi");
+}
