87a99f0224cbcc87d2e6f562e6e364540c749ae4
Tharaniben1 <tharanben193@gamil.com>
2026-02-22 22:14:20 +0530

    testing with multiple file changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log ("Welcome to git and github course");
+console.log ("Welcome to play list");
 console.log ("Welcome to git and github course");
-console.log ("Welcome to git and github course");
+for (var i = 0 ; i < 5 ; i++){
+    console.log("hello");
+}
